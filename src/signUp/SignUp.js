955e3341c49e785c1eede2cfc938a997adfa8eb3
Rasha Baig <rasha.baig@gmail.com>
2019-06-14 11:58:24 -0400

    updated url

--- src/signUp/SignUp.js
+++ src/signUp/SignUp.js
@@ -25,7 +25,7 @@ class SignUp extends Component {
 	}
 
 	handleSubmit() {
-		const URL = 'http://localhost:3001/api/foodie/newUser';
+		const URL = 'https://foodie---app.herokuapp.com/api/foodie/newUser';
 		axios
 			.post(URL, {
 				firstName: this.state.firstName,
